c16b80f3d91a100d50bb51ab2f9a7319e477ef4c
Avi_no_regio <avikrish05@gmail.com>
2026-06-10 12:32:31 +0530

    uploaded

---
+++ DAY-10/q37.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int r = 5;
+    for (int i = 1; i <= r; i++) {
+        for (int s = 1; s <= r - i; s++) {
+            printf(" ");
+        }
+        for (int j = 1; j <= 2 * i - 1; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
