2f2f3104fb651646491788f37b4872e54a7d50fb
Avi_no_regio <avikrish05@gmail.com>
2026-06-13 10:31:54 +0530

    uploaded

---
+++ DAY-12/armst_ufun.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int armstrong(int num)
+{
+    int sum = 0, rem, temp = num;
+
+    while(temp > 0)
+    {
+        rem = temp % 10;
+        sum = sum + (rem * rem * rem);
+        temp = temp / 10;
+    }
+
+    if(sum == num)
+        return 1;
+    else
+        return 0;
+}
+
+int main()
+{
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if(armstrong(num))
+        printf("%d is an Armstrong Number.", num);
+    else
+        printf("%d is not an Armstrong Number.", num);
+
+    return 0;
+}
